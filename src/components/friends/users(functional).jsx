eb3fd7c520ad02df7e722e3cf26538dbd91aadd3
Nickborovkov <borovckov@yandex.ru>
2021-04-16 17:21:36 +0300

    users component changed from functional to class

--- src/components/friends/users(functional).jsx
+++ src/components/friends/users(functional).jsx
@@ -3,27 +3,26 @@ import userPhoto from './../../assets/images/user.jpg'
 
 
 import * as axios from 'axios'
-import React from 'react'
 
 
-class Users extends React.Component {
-    getUsers = () => {
-        if(this.props.users.length === 0){
+let Users = (props) => {
+    let getUsers = () => {
+        if(props.users.length === 0){
 
             axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
-                this.props.setUsers(response.data.items)
+                props.setUsers(response.data.items)
             })
         };
     };
-    render() {
-        return <div>
+
+    return <div>
         <h2 className={classes.users__heading}>Users</h2>
 
-        <button onClick={this.getUsers}>Get Users</button>
+        <button onClick={getUsers}>Get Users</button>
         <div className={classes.user__holder}>
             
         {
-            this.props.users.map(u => <div className={classes.user__inner} key = {u.id}> 
+            props.users.map(u => <div className={classes.user__inner} key = {u.id}> 
                 <img className={classes.user__avatar} src={u.photos.small != null ? u.photos.small: userPhoto} alt="1"/>
                 <div className={classes.user__box}>
                     <p className={classes.user__property}>{u.name}</p>
@@ -37,17 +36,15 @@ class Users extends React.Component {
                     {
                         u.followed 
                         ? <button className={classes.user__button_f} 
-                        onClick={()=>{this.props.unfollow(u.id)}}>unfollow</button>
+                        onClick={()=>{props.unfollow(u.id)}}>unfollow</button>
                         : <button className={classes.user__button_u} 
-                        onClick={()=>{this.props.follow(u.id)}}>follow</button>
+                        onClick={()=>{props.follow(u.id)}}>follow</button>
                     }
                 </div>
             </div>)
         }
         </div>
     </div>
-    }
-}
-
+};
 
 export default Users;
